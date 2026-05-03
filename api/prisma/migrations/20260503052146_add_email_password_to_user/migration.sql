c7dcabb4590e4fda83ed872fd972dbbf8ea73669
tomireyes28 <tomireyes.tr@gmail.com>
2026-05-03 02:22:30 -0300

    feat(api): update prisma schema with email/password and install bcrypt

---
+++ api/prisma/migrations/20260503052146_add_email_password_to_user/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "password" TEXT,
+ADD COLUMN     "picture" TEXT,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ALTER COLUMN "name" SET NOT NULL;
